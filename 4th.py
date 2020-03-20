3784717a3c6ed9ee9f7b06036d3f21bfeee0a365
Krypton <madhusudanbabar@gmail.com>
2020-03-20 13:15:07 +0530

    basic assignments

---
+++ 4th.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 20 13:00:49 2020
+
+@author: krypton
+"""
+
+
+#%% imports
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+#%%
+
+img = cv2.imread("../DATA/00-puppy.jpg")
+
+
+#%% 
+#cv2.imshow("img", img)  
+plt.imshow(img)
+
+#%%
+
