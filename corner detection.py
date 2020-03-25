963b1cbe152bdfcece4ff7b86c5a828ff812a012
Krypton <madhusudanbabar@gmail.com>
2020-03-25 10:07:25 +0530

    harris corner detection algorithm

--- corner detection.py
+++ corner detection.py
@@ -42,3 +42,8 @@ plt.imshow(flat_chess)
 
 #############################################################################
 
+gray = np.float32(gray_real_chess)
+dst = cv2.cornerHarris(gray, 2, 3, 0.04)
+dst = cv2.dilate(dst, None)
+real_chess[dst>0.01*dst.max()] = [255, 0, 0]
+plt.imshow(real_chess)
